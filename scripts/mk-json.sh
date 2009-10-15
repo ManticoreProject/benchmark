0a234aceda302d8686a2ce655e52f62179312cd5
Adam Shaw <ams@cs.uchicago.edu>
2009-10-15 22:37:37 +0000

    script to run the sml json-maker on a given sml file

---
+++ scripts/mk-json.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# TODO check for exactly one argument
+
+NEW_BASENAME=${1%.sml}
+
+STRUCTURE_NAME=`grep "structure" $1 | cut -d " " -f 2`
+
+FULL_SML=`realpath $1`
+JSON_OUTFILE=${FULL_SML%.sml}.json
+
+(
+cat <<EOF
+Library
+  signature EXPERIMENT
+  functor MakeJSONFn
+  structure Main
+is
+  \$/basis.cm
+  common.sml
+  experiment.sig
+  make-json-fn.sml
+  new-main.sml
+  $NEW_BASENAME.sml
+EOF
+) > new-sources.cm
+
+(
+cat <<EOF
+structure Main = struct
+  structure J = MakeJSONFn ($STRUCTURE_NAME)
+  val _ = J.mkJSON "$JSON_OUTFILE"
+end
+EOF
+) > new-main.sml
+
+# compile with sml sources.cm
+echo "CM.make \"new-sources.cm\"" | sml
+
