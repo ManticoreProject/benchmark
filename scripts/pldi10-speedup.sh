292bc09bd04dd80a751e99e6bbcab383ff632e3c
Mike Rainey <mike.rainey@inria.fr>
2009-10-15 22:35:53 +0000

    Fix

--- scripts/pldi10-speedup.sh
+++ scripts/pldi10-speedup.sh
@@ -31,10 +31,10 @@ LEAF_SIZES=(
 NUM_TRIALS=8
 
 for (( i = 0 ; i < ${#PARALLEL_BENCHMARKS[@]} ; i++ )); do
-    ./experiment.sh -s ${INPUT_SIZES[i]} -a ${PARALLEL_BUILD_RULES[i]} -f ../programs/${PARALLEL_BENCHMARKS[i]} -l ${LEAF_SIZES} -n ${PARALLEL_BENCHMARKS[i]} -t $NUM_TRIALS -p 1 -p 2 -p 3 -p 4 -p 5 -p 6 -p 7 -p 8 -p 9 -p 10 -p 11 -p 12 -p 13 -p 14 -p 15 -p 16
+    ./experiment.sh -s ${INPUT_SIZES[i]} -a ${PARALLEL_BUILD_RULES[i]} -f ../programs/${PARALLEL_BENCHMARKS[i]} -l ${LEAF_SIZES[i]} -n ${PARALLEL_BENCHMARKS[i]} -t $NUM_TRIALS -p 1 -p 2 -p 3 -p 4 -p 5 -p 6 -p 7 -p 8 -p 9 -p 10 -p 11 -p 12 -p 13 -p 14 -p 15 -p 16
 done
 
 for (( i = 0 ; i < ${#SEQUENTIAL_BENCHMARKS[@]} ; i++ )); do
-    ./experiment.sh -s ${INPUT_SIZES[i]} -c ${SEQUENTIAL_BUILD_RULES[i]} -f ../programs/${SEQUENTIAL_BENCHMARKS[i]} -l ${LEAF_SIZES} -n ${SEQUENTIAL_BENCHMARKS[i]} -t $NUM_TRIALS -p 1
+    ./experiment.sh -s ${INPUT_SIZES[i]} -c ${SEQUENTIAL_BUILD_RULES[i]} -f ../programs/${SEQUENTIAL_BENCHMARKS[i]} -l ${LEAF_SIZES[i]} -n ${SEQUENTIAL_BENCHMARKS[i]} -t $NUM_TRIALS -p 1
 done
 
