43cf7953ae6bb54565c34ef3b7fc2ed975d3d65f
Adam Shaw <ams@cs.uchicago.edu>
2009-10-30 15:22:51 +0000

    m

--- benchmarks/scripts/bz2-to-db.sh
+++ benchmarks/scripts/bz2-to-db.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# Set ENABLED to "true" to enable the script, "false" to disable it.
+# The disabled script reports on what it would do.
+
+# ENABLED="true"
+ENABLED="false"
+
+if [[ "$ENABLED" == "false" ]] ; then
+  echo "----- NOTE: this script is disabled"
+  echo "----- in script text, set ENABLED to true to enable"
+fi
+
+maybe_do () {
+  CMD=$*
+  if [[ "$ENABLED" == "true" ]] ; then 
+    $CMD
+  elif [[ "$ENABLED" == "false" ]] ; then 
+    echo "(diabled, skipping) $CMD"
+  else
+    echo "unexpected value of ENABLED"
+  fi
+}
+
 ZIPPED=`realpath $1` 
 UNZIPPED=${ZIPPED%.tar.bz2}
 ENCLOSING=`dirname $ZIPPED`
@@ -7,36 +29,27 @@ ENCLOSING=`dirname $ZIPPED`
 echo "----- full path to archive: $ZIPPED"
 
 # read off the manti-bench path from $ZIPPED
-P=$ZIPPED
-D=""
-MBENCH=""
-until [ "$D" == "manti-bench" ] ; do
-  D=${P%%/*} # D is everything before first slash
-  P=${P#*/}  # P is everything after first slash
-  if [ "$D" != "" ]
-    then MBENCH="$MBENCH/$D"
-  fi
-done
 
+MBENCH="${ZIPPED%manti-bench*}manti-bench"
 echo "----- read off path to manti-bench: $MBENCH"
 
 # unzip the file
 echo "----- unpacking $1"
 cd $ENCLOSING
-echo "tar xvjf $ZIPPED"
+maybe_do "tar xvjf $ZIPPED"
 
 cd $MBENCH/benchmarks/manticore-db/src/sml
 
 echo "----- making json files from sml"
-echo "mk-json.sh $UNZIPPED/*.sml"
+maybe_do "mk-json.sh $UNZIPPED/*.sml"
 
 cd $MBENCH/benchmarks/manticore-db/src/java
 
 echo "----- pushing json files into the database"
-echo "./compile.sh"
-echo "./run.sh $UNZIPPED/*.json"
+maybe_do "./compile.sh"
+maybe_do "./run.sh $UNZIPPED/*.json"
 
 echo "----- removing $UNZIPPED"
-rm -rf $UNZIPPED
+maybe_do "rm -rf $UNZIPPED"
 
 echo "----- done"
