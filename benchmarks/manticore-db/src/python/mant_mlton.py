f17fd81e09efc33ca546eed1fb551095219dc472
Mike Rainey <mike.rainey@inria.fr>
2009-11-09 20:29:30 +0000

    Swp vs trunk plot.

--- benchmarks/manticore-db/src/python/mant_mlton.py
+++ benchmarks/manticore-db/src/python/mant_mlton.py
@@ -1,80 +1,4 @@
-# Adam Shaw, November 2009
-
-# Please see EDIT ME! below for customization.
-
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import collect_data
-import utils
-
-### Cosmetics
-
-h1 = fm.FontProperties()
-h1.set_size(16)
-
-h2 = fm.FontProperties()
-h2.set_size(12)
-
-h3 = fm.FontProperties()
-h3.set_size(10)
-
-for h in [h1, h2, h3]:
-  h.set_family('Times New Roman')
-
-### Plotting
-
-# ratio_plot : (string, float) list  -> _
-# each pair is
-# - a problem name (e.g. mandelbrot), and
-# - a mlton/mant ratio
-def ratio_plot(pairs):
-  (names, ratios) = utils.unzip(pairs)
-  print "names:"
-  print names
-  print "ratios:"
-  print ratios
-  N = len(names)
-  w = 0.3
-  ind = np.arange(N)
-  h = math.ceil(max(ratios)) + 0.1
-  # plot a solid horizontal line at y=1
-  plt.axhline(y=1, xmin=0, xmax=N, color='gray')
-  plt.bar(ind, ratios, width=w, color='dodgerblue')
-  plt.title('Sequential Manticore time / MLton time for various programs', fontproperties=h1)
-  plt.xticks(ind+w/2.0, names, fontproperties=h2)
-  plt.yticks(np.arange(0, h), fontproperties=h2)
-  plt.ylabel('mant / mlt', fontproperties=h1)
-  for x in range(0, len(names)):
-    y = ratios[x]
-    ystr = "%.2f" % y
-    plt.annotate(ystr,
-                 xy=(x,y),
-                 xytext=(x+0.07,y+0.05),
-                 fontproperties=h2)
-  plt.show()
-
-# ratios : (int * int * string) list -> (string * float) list
-# Each input triple is 
-# - a sequential manticore context id,
-# - a mlton context id, and
-# - a problem name
-# Returns pairs of
-# - the problem name, and
-# - the ratio of median seq mant time to median mlton time
-def ratios(triples):
-  (mants, mlts, names) = utils.unzip3(triples)
-  retval = []
-  for t in triples:
-    mant=t[0]
-    mlton=t[1]
-    name=t[2]
-    t_mant = collect_data.med_baseline_time(mant)
-    t_mlton = collect_data.med_time(mlton)   
-    r = t_mant / t_mlton
-    retval.append((name, r))
-  return retval
+import ratio_plot
 
 # EDIT ME!
 # Each triple is a seq manticore context id, a mlton context id, and a name
@@ -84,4 +8,6 @@ triples = [(605, 536, 'plus-scan'),
            (609, 544, 'quickhull'),
            (611, 548, 'pquicksort')]
       
-ratio_plot(ratios(triples))
+ratio_plot.ratio_plot('Sequential Manticore time / MLton time for various programs',
+                      'mant / mlt',
+                      ratio_plot.ratios(triples))
---
+++ benchmarks/manticore-db/src/python/swp-vs-trunk.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+# Adam Shaw, Mike Rainey
+# Nov 2009
+
+import collect_data as get
+import speedup as s
+import utils
+import ratio_plot
+
+# extract_benchmark_name : string -> string
+# extract the benchmark name from the given url
+# ex: extract_benchmark_name('https://foo/bar/baz/scott-baio') ==> 'scott-baio'
+# pre: the input is a well-formed url
+def extract_benchmark_name(url):
+  toks = url.split('/')
+  toks.reverse()
+  return(toks[0])
+
+# findit : (int * string * string) * (int * string * string) list -> int or False
+# find the sequential baseline for the given benchmark name
+# the arguments are
+# - a context_id, url and compiler branch for a benchmark
+# - a list of context_ids, urls and compiler branches for sequential benchmarks
+# a baseline is considered found when its url matches the par url exactly
+def findit(bench, seqs):
+  for b in seqs:
+    seq_id, seq_url, seq_branch = b
+    bench_id, bench_url, bench_branch = bench
+    if (bench_url == seq_url):
+      return(seq_id)
+  # if you make it this far...
+  return(False)
+
+triples = []
+swps = get.most_recent_seqs(get.SWP)
+trunks = get.most_recent_seqs(get.Trunk)
+for b in swps:
+  swp_id, url, branch = b
+  bench_name = extract_benchmark_name(url)
+  trunk_id = findit(b, trunks)
+  if (trunk_id != False):
+    triples.append((trunk_id, swp_id, bench_name))
+  else:
+    print (bench_name + ":\tdid not find baseline")
+
+print triples
+
+# Go!
+ratio_plot.ratio_plot('Sequential Trunk / Sequential Swp',
+                      'trunk / swp',
+                      ratio_plot.ratios(triples))
+
+
+
+
