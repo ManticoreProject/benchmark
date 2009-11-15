d0d786d7f4c8dd18638de2746f16b28f5ec53e48
Mike Rainey <mike.rainey@inria.fr>
2009-11-15 22:55:20 +0000

    Edits.

--- benchmarks/manticore-db/src/python/pldi10_benchmarks.py
+++ benchmarks/manticore-db/src/python/pldi10_benchmarks.py
@@ -1,14 +1,26 @@
-benchmarks=[
-     'barnes-hut',
-     'pfib',
-     'id-raytracer',
-     'mandelbrot',
-     'ttt',
-     'plus-reduce',
-     'plus-scan',
-     'pmergesort',
-     'pquicksort',
-     'pquickhull',
-     'smvm',
-     'tree-add'
-     ]
+benchmark_data=[
+     ("barnes-hut", "barnes-hut"),
+     ("pfib", "fib"),
+     ("id-raytracer", "raytracer"),
+     ("mandelbrot", "mandelbrot"),
+     ("ttt", "ttt"),
+     ("plus-reduce", "plus-reduce"),
+     ("plus-scan", "plus-scan"),
+     ("pmergesort", "mergesort"),
+     ("pquicksort", "quicksort"),
+     ("pquickhull", "quickhull"),
+     ("smvm", "smvm"),
+     ("tree-add", "tree-add")
+]
+
+def fst(tup):
+  return(tup[0])
+
+benchmarks=map(fst, benchmark_data)
+
+def pretty_name(benchmark):
+  for b, n in benchmark_data:
+      if b == benchmark:
+          return(n)
+  raise Exception("nonexistant benchmark name " + benchmark)
+
--- benchmarks/manticore-db/src/python/swp-vs-trunk.py
+++ benchmarks/manticore-db/src/python/swp-vs-trunk.py
@@ -10,7 +10,9 @@ import ratio_plot
 import branches
 import pldi10_benchmarks
 
-print ('benchmark\t\tdelta time (%)')
+print ('\\begin{tabular}{l r}')
+print ('Program & \% Slowdown\\\\')
+print ('\\hline')
 for b in pldi10_benchmarks.benchmarks:
   experiment_id=get.most_recent_experiment(b)
   swp_seq_id=get.most_recent_pml_bench(experiment_id, branches.SWP, 'true')
@@ -19,6 +21,7 @@ for b in pldi10_benchmarks.benchmarks:
     swp_time = get.med_baseline_time(swp_seq_id[0])
     trunk_time = get.med_baseline_time(trunk_seq_id[0])
     if swp_time > trunk_time:
-      print (b + '\t\t+%.2f'%((1.0 - trunk_time / swp_time) * 100))
+      print (pldi10_benchmarks.pretty_name(b) + ' & +%.2f'%((1.0 - trunk_time / swp_time) * 100) + '\\\\')
     else:
-      print (b + '\t\t+%.2f'%((1.0 - swp_time / trunk_time) * 100))
+      print (pldi10_benchmarks.pretty_name(b) + ' & -%.2f'%((1.0 - swp_time / trunk_time) * 100) + '\\\\')
+print ('\\end{tabular}')
