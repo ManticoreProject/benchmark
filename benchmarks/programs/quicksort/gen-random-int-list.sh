117984517d798826c011289f794caff846f84c78
Lars Bergstrom <lars@lars.com>
2010-10-28 12:59:49 +0000

    gen-random-int-list files

---
+++ benchmarks/programs/quicksort/gen-random-int-list.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+LIST_LEN=$1
+if [ "$LIST_LEN" == "" ]; then
+    LIST_LEN="1000000"
+fi
+SMLNJ="sml"
+
+if [ ! -f ../../input-data/random-int-list.txt ]; then
+   echo "SMLofNJ.exportFn(\"gen-random-int-list\", Main.main);" | $SMLNJ gen-random-int-list.cm
+   $SMLNJ @SMLload=gen-random-int-list $LIST_LEN file > ../../input-data/random-int-list.txt
+fi
