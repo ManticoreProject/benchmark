7a1c5bf0529aaf937a7e56bb27e4b61b45213375
Matthew Le <ml9951@rit.edu>
2015-01-06 20:16:31 +0000

    adding R script for processing times for STM benchmarks

---
+++ benchmarks/scripts/processTimes.R
@@ -0,0 +1,29 @@
+
+args <- commandArgs(trailingOnly = TRUE)
+if(length(args) == 1){
+    data = read.table(args[1])
+}else{
+    stop("Usage: Rscript processTimes.R <filename>")
+}
+
+
+partial = data[1:40,]
+full = data[-(1:40),]
+
+pTimes = partial[c(TRUE,FALSE,FALSE,FALSE)]
+partialAborts = partial[c(FALSE,TRUE,TRUE,TRUE)]
+fullAborts = partial[c(FALSE,FALSE,TRUE,FALSE)]
+pAborts = partial[c(FALSE,FALSE,FALSE,TRUE)]
+
+fTimes = full[c(TRUE,FALSE)]
+fAborts = full[c(FALSE,TRUE)]
+
+cat("Partial abort time =", mean(pTimes), "\n")
+cat("Full abort time =", mean(fTimes), "\n")
+percent = ((mean(fTimes) - mean(pTimes)) / mean(fTimes)) * 100
+cat("Partial is ", percent, "% faster than full abort\n", sep="")
+cat("Partially aborted", mean(partialAborts), "transactions\n")
+cat("Fully aborted", mean(fullAborts), "transactions\n")
+cat("Aborted", mean(pAborts), "transactions in total\n")
+cat("Full abort implementation aborts", mean(fAborts), "transactions\n")
+
