77908340ba02d43db56b46add3b58e381f2cf103
Matthew Le <ml9951@rit.edu>
2015-01-28 18:37:00 +0000

    updating

--- benchmarks/scripts/processFile.py
+++ benchmarks/scripts/processFile.py
@@ -93,6 +93,18 @@ def isCommon(key, lists):
             return False
     return True
 
+def reduce(function, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        try:
+            initializer = next(it)
+        except StopIteration:
+            raise TypeError('reduce() of empty sequence with no initial value')
+    accum_value = initializer
+    for x in it:
+        accum_value = function(accum_value, x)
+    return accum_value
+
 def mean(l):
     return reduce(lambda x, y: x + y, l) / len(l)
 
