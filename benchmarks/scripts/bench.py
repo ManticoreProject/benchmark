9998c672b98d2cbda8db36988f3cf122002eb06e
Matthew Le <ml9951@rit.edu>
2015-06-17 13:53:44 +0000

    updating scripts

--- benchmarks/scripts/bench.py
+++ benchmarks/scripts/bench.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import sys, getopt, re, os
+import sys, getopt, re, os, signal
 import subprocess
 import argparse
-
+from time import gmtime, strftime
 
 parser = argparse.ArgumentParser()
 
@@ -14,8 +14,12 @@ parser.add_argument("-threads", type=int, help="Number of threads to use", defau
 args = parser.parse_args()
 
 stms = ["ffnorec", "norec", "orderedNoRec", "pnorec", "bounded", "full", "partial", "ordered", "tailff", "ff", "ffRefCount", "ffRefCountGC"]
+benchmarks = ["linked-list-stm", "red-black-stm"]
+
 
 def runSTM(stm, file):
+	subprocess.Popen('echo \"name = ' + stm + '\" >> ' + file + ';', shell=True).wait()
+	subprocess.Popen('pmlc mc.mlb', shell=True).wait()
 	for i in range(args.iters):
 		print('------' + stm + ' Iteration ' + str(i) + '------')
 		try:
@@ -23,31 +27,55 @@ def runSTM(stm, file):
 			while res1 != 0:
 				print('execution finished with return code: ' + str(res1))
 				res1 = subprocess.Popen('./a.out -stm ' + stm + ' -p ' + str(args.threads) + ' > currentTime.txt', shell = True).wait()
-			subprocess.Popen('cat currentTime.txt; echo \"name = ' + stm + '\" >> ' + file + '; cat currentTime.txt >> ' + file + '; echo \"end\" >> ' + file, shell = True).wait()
+			subprocess.Popen('cat currentTime.txt;  cat currentTime.txt >> ' + file + '; echo \"end\" >> ' + file, shell = True).wait()
 		except Exception as e:
 			print("Exception raised: " + str(e))
 			return
 
-def benchmark():
+filename = None
+
+def benchmark(program):
+	os.chdir('../programs/' + program)
+	if not(os.path.isdir("benchmark-times")):
+		os.mkdir('benchmark-times')
+	global filename
+	filename = "benchmark-times/times-" + strftime("%Y-%m-%d %H:%M:%S", gmtime()).replace(' ', '-') + '.txt'
 	if args.run == "all":
-		os.system('echo "" > times.txt')
+		os.system('echo "" > ' + filename)
 		for stm in stms:
-			runSTM(stm, 'times.txt')
+			runSTM(stm, filename)
 	else:
 		os.system('echo "" > ' + args.run + 'Times.txt')
 		runSTM(args.run, args.run + 'Times.txt')
 
 def runBenchmarks():
-	currentDir = os.getcwd()
+	global filename
+	originalDirectory = os.path.dirname(os.path.realpath(__file__))
+	os.chdir(originalDirectory)
 	if args.program is None:
-		return			
+		for bench in benchmarks:
+			benchmark(bench)
+			os.chdir(originalDirectory)
 	else:
-		os.chdir('../programs/' + args.program)
-		benchmark()
-		os.chdir(currentDir)
+		benchmark(args.program)
+		os.chdir(originalDirectory)
     
+def signal_handler(signal, frame):
+	print("Handling Ctrl+C")
+	if os.path.exists(filename):
+		while True:	
+			result = input('Delete ' + filename + '?')
+			if result == 'yes':
+				os.remove(filename)
+				sys.exit(0)
+			elif result == 'no':
+				sys.exit(0)
+			else:
+				print('Answer \"yes\"" or \"no\"')
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def main():
-    runBenchmarks()
+	runBenchmarks()
 if __name__ == "__main__":
     main()    
--- benchmarks/scripts/processFile.py
+++ benchmarks/scripts/processFile.py
@@ -1,203 +1,39 @@
 #!/usr/bin/env python
-import sys, getopt, re, os
+import sys, getopt, re, os, pdb
 import statistics as st
-from collections import OrderedDict
-import pdb, traceback
 
-baseline = "full"
+#use pdb.set_trace() to set a breakpoint
 
-def getFlag(f):
-    it = iter(sys.argv)
-    for arg in it:
-        if arg == f:
-            return next(it)
-    return None
-
-def getFile():
-    for arg in sys.argv:
-        filename, ext = os.path.splitext(arg)
-        if ext == '.txt':
-            return arg
-    return None
-
-def getOpt(f):
-    for arg in sys.argv:
-        if arg == f:
-            return True
-    return False
-
-def processFile(f):
-    
-    numCatagories = 0
-    lists = {}
-
-    def processItem(it, data):
-        nl = next(it)
-        nl = nl.strip()
-        while nl != "end":
-            keyVal = nl.split("=")
-            if len(keyVal) != 2:
-                nl = next(it).strip()
-                continue
-            data[keyVal[0].strip()].append(float(keyVal[1].strip()))
-            nl = next(it).strip()
-        return data
-
-    def processNewItem(it):
-        nl = next(it)
-        nl = nl.strip()
-        data = {}
-        while nl != "end":
-            keyVal = nl.split("=")
-            if len(keyVal) != 2:
-                nl = next(it).strip()
-                continue
-            data[keyVal[0].strip()] = [float(keyVal[1].strip())]
-            nl = next(it).strip()
-        return data
-
-    fh = open(f)
-
-    nl = next(fh, "__done__")
-
-    while nl != "__done__":
-        if re.match(r'^\s*$', nl):
-            nl = next(fh, "__done__")
-            continue
-        nl = nl.strip()
-        keyVal = nl.split("=")
-        if len(keyVal) == 2 and keyVal[0].strip() == "name":
-            name = keyVal[1].strip()
-        else:
-            nl = next(fh, "__done__")
-            continue
-        if name in lists:
-            processItem(fh, lists[name])
-        else:
-            lists[name] = processNewItem(fh)
-            
-        nl = next(fh, "__done__")
-    return(lists)
-
-def reduce(function, iterable, initializer=None):
-    it = iter(iterable)
-    if initializer is None:
+def toValue(v):
+    try:
+        return int(v)
+    except ValueError:
         try:
-            initializer = next(it)
-        except StopIteration:
-            raise TypeError('reduce() of empty sequence with no initial value')
-    accum_value = initializer
-    for x in it:
-        accum_value = function(accum_value, x)
-    return accum_value
+            return float(v)
+        except ValueError:
+            return v
 
-
-def printData(lists):
-    for n in lists:
-        lists[n] = OrderedDict(sorted(lists[n].items()))
-
-    lists = OrderedDict(sorted(lists.items()))
-
-
-    def isCommon(key, lists):
-        keys = map(lambda x : lists[x].keys(), lists)
-        for ks in keys:
-            if (key not in ks):
-                return False
-        return True
-
-    
-
-    def mean(l):
-        return reduce(lambda x, y: x + y, l) / len(l)
-
-    def mkString(lists, k):
-        s = k
-        for l in lists:
-            l = lists[l]
-            if k in l:
-                s += ' & ' + ("%.2f" % mean(l[k])) 
-            else:
-                s += ' & - '
-        s += '\\\\\\hline'
-        return s
-            
-    def mkComparisonString(lists, k):
-        s = k + "$\\Delta$"
-        base = mean(lists[baseline][k])
-        for key in lists:
-            l = lists[key]
-            if base == 0:
-                change = 0
-            else:
-                change = ((base - mean(l[k])) / base) * 100
-            s += ' & ' + ("%.2f" % change) + "\%"
-        s += '\\\\\\hline'
-        return s
-            
-
-    if getOpt("-tex"):
-        print('\\begin{figure}')
-        print('\\noindent\\adjustbox{max width=\\textwidth}{%')
-        print('\\begin{tabular}{|' + reduce (lambda x, y: ' c |' + x, range((len(lists)+1) ), '') + '}')
-        print("\\hline")
-        print (reduce (lambda x, y: x + ' & '+ y , lists.keys(), '') + '\\\\\\hline')
-        iters = 0
-        doneKeys = []
-        for k in lists:
-            l = lists[k]
-            for k in l:
-                if k in doneKeys:
-                    continue
-                iters = len(l[k])
-                print(mkString(lists, k))
-                if isCommon(k, lists) and baseline in lists:
-                    print(mkComparisonString(lists, k))
-                doneKeys.append(k)    
-        print("\\end{tabular}")
-        print("}")
-        caption = getFlag("-caption")
-        if caption is not None:
-            print ("\\caption{" + caption + " (Average of " + str(iters) + " iterations, baseline: " + baseline + ")}")
-        else:
-            print ("\\caption{Average of " + str(iters) + " iterations (baseline: " + baseline + ")}")
-        print('\\end{figure}')
-    else:
-        for key in lists:
-            print (key + ":")
-            l = lists[key]
-            for key in l:
-                mean = reduce(lambda x, y: x + y, l[key]) / len(l[key])
-                print ("\tMean " + key + ": " + str(mean))
+def processChunk(chunk):
+    lines = chunk.split('\n')
+    chunkDict = {}
+    for l in lines:
+        keyVal = [x.strip() for x in l.split('=')]
+        if len(keyVal) == 2:
+            chunkDict.setdefault(keyVal[0], []).append(toValue(keyVal[1]))
+    return chunkDict
 
 def main():
-    if len(sys.argv) < 2:
-        print("usage: python processFile <output file>")
-
-    baseline = "full"
-    f = getFile()
-    if f is None:
-        print("No file specified")
-        sys.exit(1)
-    data = processFile(f)
-    printData(data)
+    data = {}
+    f = open('txt')
+    text = f.read()
+    stmChunks = re.split('name = .*', text)
+    stmChunks = [chunk for chunk in stmChunks if chunk.rstrip()]
+    stms = re.findall('name = .*', text)
+    for stm, chunk in zip(stms, stmChunks):
+        chunkDict = processChunk(chunk)
+        whichSTM= [x.strip() for x in stm.split('=')]
+        data[whichSTM[1]] = chunkDict
+        pdb.set_trace()
 
 if __name__ == "__main__":
-    try:
-        main()    
-    except:
-        type, value, tb = sys.exc_info()
-        traceback.print_exc()
-        pdb.post_mortem(tb)
-
-
-
-
-
-
-
-
-
-
-
-    
+    main()    
