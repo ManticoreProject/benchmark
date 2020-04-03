c99fd7fb229713789529c2f270156a64459f40da
Kavon Farvardin <kavon@farvard.in>
2020-04-03 18:25:27 -0500

    fix unbound var error

--- plotting/plot.py
+++ plotting/plot.py
@@ -143,11 +143,11 @@ def sortStacks(cats):
 def float2lab(x):
     return str(decimal.Decimal("{:.2f}".format(x)).normalize())
 
-def compressYTickLabels(yLabelTexts):
+def compressYTickLabels(yLabelTexts, dpi_scale_trans):
     # Create offset transform to push the program labels to save space.
     dx = 10/72.
     dy = 0/72.
-    offset = matplotlib.transforms.ScaledTranslation(dx, dy, g.fig.dpi_scale_trans)
+    offset = matplotlib.transforms.ScaledTranslation(dx, dy, dpi_scale_trans)
     for label in yLabelTexts:
         label.set_horizontalalignment('right')
         label.set_fontsize(14)
@@ -445,7 +445,7 @@ def relative_time(df, baseline, dir, xmax, subset=None, filename="running_time.p
     for ax in g.axes.flat:
         # add an "x" after the labels for speedup
         ax.xaxis.set_major_formatter(FuncFormatter(formatLabel))
-        compressYTickLabels(ax.yaxis.get_ticklabels())
+        compressYTickLabels(ax.yaxis.get_ticklabels(), g.fig.dpi_scale_trans)
 
 
 
@@ -550,7 +550,7 @@ def cachegrind_event_pct(df, event_name, numerator_s, denominator_s, dir, codeCa
     for ax in g.axes.flat:
         # set x axis to use the percent formatter
         ax.xaxis.set_major_formatter(PercentFormatter(xmax=100))
-        compressYTickLabels(ax.yaxis.get_ticklabels())
+        compressYTickLabels(ax.yaxis.get_ticklabels(), g.fig.dpi_scale_trans)
 
     plt.xlim(xBounds)
 
@@ -657,7 +657,7 @@ def gc_plot(df, dir, numerator_s, denominator_s, event_name, subset=None, \
         for ax in g.axes.flat:
             # set x axis to use the percent formatter
             ax.xaxis.set_major_formatter(PercentFormatter(xmax=100))
-            compressYTickLabels(ax.yaxis.get_ticklabels())
+            compressYTickLabels(ax.yaxis.get_ticklabels(), g.fig.dpi_scale_trans)
 
     plt.xlim(xBounds)
 
