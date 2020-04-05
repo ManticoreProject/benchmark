1190e58d2fe97ccd63d600cd3551b2e499a51d0d
Kavon Farvardin <kavon@farvard.in>
2020-04-05 01:15:34 -0500

    input data generation segfaults on 64-bit smlnj for barneshut

--- benchmarks/programs/barnes-hut/gen-bodies.sh
+++ benchmarks/programs/barnes-hut/gen-bodies.sh
@@ -7,8 +7,11 @@ if [ "$LIST_LEN" == "" ]; then
     LIST_LEN="400000"
 fi
 SMLNJ="sml"
+MLTON="mlton"
 
 if [ ! -f ../../input-data/bodies.txt ]; then
-   echo "SMLofNJ.exportFn(\"gen-bodies\", Main.main);" | $SMLNJ gen-bodies.cm
-   $SMLNJ @SMLload=gen-bodies $LIST_LEN file > ../../input-data/bodies.txt
+   # echo "SMLofNJ.exportFn(\"gen-bodies\", Main.main);" | $SMLNJ gen-bodies.cm
+   # $SMLNJ @SMLload=gen-bodies $LIST_LEN file > ../../input-data/bodies.txt
+   $MLTON -output gen-input gen-input-data.mlb
+   ./gen-input $LIST_LEN file > ../../input-data/bodies.txt
 fi
